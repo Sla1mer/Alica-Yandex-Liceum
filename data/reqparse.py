ce392a77b51e93ea34dfe77a2f2015135a5d44ac
Sla1merAPK <slameryt4@gmail.com>
2022-04-01 18:46:30 +0200

    final release

--- data/reqparse.py
+++
@@ -1,8 +0,0 @@
-from flask_restful import reqparse
-
-parser = reqparse.RequestParser()
-parser.add_argument('name', required=True)
-parser.add_argument('surnmae', required=True)
-parser.add_argument('age', required=True, type=bool)
-parser.add_argument('is_published', required=True, type=bool)
-parser.add_argument('user_id', required=True, type=int)
--- main.py
+++ main.py
@@ -1,26 +1,15 @@
-from flask import Flask, make_response, jsonify
-from flask_restful import Api
+import os
 
-from data import db_session, news_api, jobs_api, users_resources
+from flask import Flask
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
-api = Api(app)
 
 
-def main():
-    db_session.global_init("db/blogs.db")
-    app.register_blueprint(news_api.blueprint)
-    app.register_blueprint(jobs_api.blueprint)
-
-    # для списка объектов
-    api.add_resource(users_resources.UsersResource, '/api/v2/users')
-
-    # для одного объекта
-    api.add_resource(users_resources.UsersListResource, '/api/v2/users/<int:user_id>')
-
-    app.run()
+@app.route("/")
+def index():
+    return "Привет от приложения Flask"
 
 
 if __name__ == '__main__':
-    main()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
